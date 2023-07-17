6cdfff26c87d676e4061f53f951d59c2bf6abb84
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-07-18 02:13:47 +0900

    refactor: add logger to meilisearch container

--- src/main/java/io/vanslog/testcontainers/meilisearch/MeilisearchContainer.java
+++ src/main/java/io/vanslog/testcontainers/meilisearch/MeilisearchContainer.java
@@ -1,6 +1,9 @@
 package io.vanslog.testcontainers.meilisearch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.utility.DockerImageName;
 
 /**
@@ -11,8 +14,9 @@ import org.testcontainers.utility.DockerImageName;
  */
 public class MeilisearchContainer extends GenericContainer<MeilisearchContainer> {
 
-  private static final int MEILISEARCH_DEFAULT_PORT = 7700;
+  Logger log = LoggerFactory.getLogger(MeilisearchContainer.class);
 
+  private static final int MEILISEARCH_DEFAULT_PORT = 7700;
   private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("getmeili/meilisearch");
   private static final String DEFAULT_IMAGE_TAG = "latest";
 
@@ -30,5 +34,6 @@ public class MeilisearchContainer extends GenericContainer<MeilisearchContainer>
   public MeilisearchContainer(DockerImageName dockerImageName) {
     super(dockerImageName);
     this.addExposedPort(MEILISEARCH_DEFAULT_PORT);
+    this.withLogConsumer(new Slf4jLogConsumer(log));
   }
 }
